b99dfd6626e638dfc786069f13f02686217134bb
Jennifer <jmarthagodfrey@gmail.com>
2025-02-03 11:06:32 -0700

    feat: state for form to img

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,20 @@
+import { useState } from 'react';
+
 export default function Main() {
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    function handleChange(event) {
+        const {value, name} = event.currentTarget
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
     return (
         <main className="mx-auto p-9 max-w-2xl">
             <div className="grid grid-cols-2 gap-4 mb-4">
@@ -9,6 +25,8 @@ export default function Main() {
                         placeholder="One does not simply"
                         name="topText"
                         className="w-full mt-1 rounded border border-gray-300 indent-1.5 min-h-10 font-karla"
+                        onChange={handleChange}
+                        value={meme.topText}
                     />
                 </label>
 
@@ -19,6 +37,8 @@ export default function Main() {
                         placeholder="Walk into Mordor"
                         name="bottomText"
                         className="w-full mt-1 rounded border border-gray-300 indent-1.5 min-h-10 font-karla"
+                        onChange={handleChange}
+                        value={meme.bottomText}
                     />
                 </label>
                 <button className="col-span-2 rounded bg-green-700 font-semibold text-white border-none cursor-pointer min-h-10 font-karla">
@@ -28,14 +48,14 @@ export default function Main() {
             <div className="grid grid-cols-2 gap-4">
                 <div className="col-span-2 relative flex flex-col justify-center items-center">
                     <img
-                        src="http://i.imgflip.com/1bij.jpg"
+                        src={meme.imageUrl}
                         className="w-full rounded-sm"
                     />
-                    <span className="absolute top-0 text-center my-3.5 px-1.5 font-impact text-3xl uppercase text-white tracking-wide text-outline">
-                        One does not simply
+                    <span className="absolute top-0 text-center my-3.5 px-1.5 font-impact text-3xl font-bold uppercase text-white tracking-wide text-outline">
+                        {meme.topText}
                     </span>
-                    <span className="absolute bottom-0 text-center my-3.5 px-1.5 font-impact text-3xl uppercase text-white tracking-wide text-outline">
-                        Walk into Mordor
+                    <span className="absolute bottom-0 text-center my-3.5 px-1.5 font-impact text-3xl font-bold uppercase text-white tracking-wide text-outline">
+                        {meme.bottomText}
                     </span>
                 </div>
             </div>
