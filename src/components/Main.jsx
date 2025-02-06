2c71d95d264fbf516b09894bee6b228890898afb
Jennifer <jmarthagodfrey@gmail.com>
2025-02-06 13:00:44 -0700

    feat: get random meme and display

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,13 +2,15 @@ import { useState, useEffect } from 'react';
 
 export default function Main() {
     const [meme, setMeme] = useState({
-        topText: "one does not simply",
-        bottomText: "walk into mordor",
+        topText: "",
+        bottomText: "",
         imageUrl: "http://i.imgflip.com/1bij.jpg"
     })
 
+    //stores meme urls in array 
     const [memeArray, setMemeArray] = useState([])
     
+    //fetch meme url from api
     function getMemeArray() {
         fetch('https://api.imgflip.com/get_memes')
             .then(res => {
@@ -25,16 +27,30 @@ export default function Main() {
             })
     }
 
+    //gets array of memes when component mounts
     useEffect(() => {
         getMemeArray();
     }, []);
-    
+
+    //handles form input on change
     function handleChange(event) {
         const {value, name} = event.currentTarget
         setMeme(prevMeme => ({
             ...prevMeme,
             [name]: value
         }))
+    }
+
+    //gets random meme url on button click
+    function handleClick() {
+        if (memeArray.length > 0) {
+            const randomIndex = Math.floor(Math.random() * memeArray.length)
+            const randomMemeUrl = memeArray[randomIndex]
+            setMeme(prevMeme => ({
+                ...prevMeme,
+                imageUrl: randomMemeUrl
+            }))
+        }
     }
 
 
@@ -64,7 +80,9 @@ export default function Main() {
                         value={meme.bottomText}
                     />
                 </label>
-                <button className="col-span-2 rounded bg-green-700 font-semibold text-white border-none cursor-pointer min-h-10 font-karla">
+                <button 
+                    onClick={handleClick}
+                    className="col-span-2 rounded bg-green-700 font-semibold text-white border-none cursor-pointer min-h-10 font-karla">
                     Get a new meme image 🖼
                 </button>
             </div>
